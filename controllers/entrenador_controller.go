c94b6f6b5db5b7711477decce24a5a2937202691
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:18:09 -0500

    se agrega func 12

--- controllers/entrenador_controller.go
+++ controllers/entrenador_controller.go
@@ -127,3 +127,14 @@ func UpdateEntrenador(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, map[string]string{"message": "Entrenador actualizado correctamente"})
 }
+
+
+func DeleteEntrenador(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	_, err := config.DB.Exec("DELETE FROM entrenadores WHERE id_entrenadores=$1", id)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, map[string]string{"message": "Entrenador eliminado correctamente"})
+}
