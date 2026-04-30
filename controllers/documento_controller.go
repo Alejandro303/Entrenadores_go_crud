94eb04ea588bea3fa2047419ef8f32a2de325983
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:12:18 -0500

    se agrega func 3

--- controllers/documento_controller.go
+++ controllers/documento_controller.go
@@ -38,3 +38,27 @@ func GetAllDocumentos(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, lista)
 }
+
+
+func GetDocumentoByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var d models.EntrenadorDocumento
+
+	err := config.DB.QueryRow(
+		`SELECT id, entrenador_id, identificacion, nombre_archivo, creado_en,
+		activo, fecha_modificacion, fecha_creacion FROM entrenador_documentos WHERE id=$1`, id,
+	).Scan(
+		&d.ID, &d.EntrenadorID, &d.Identificacion, &d.NombreArchivo,
+		&d.CreadoEn, &d.Activo, &d.FechaModificacion, &d.FechaCreacion,
+	)
+
+	if err == sql.ErrNoRows {
+		respondJSON(w, 404, map[string]string{"error": "Documento no encontrado"})
+		return
+	}
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, d)
+}
