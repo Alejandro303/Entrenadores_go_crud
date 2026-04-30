bf93b19f94b90b1f9e0eadea776a5aa21630a12e
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:19:38 -0500

    se agrega func 13

---
+++ controllers/solicitud_controller.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"API_ENTRENADORES/config"
+	"API_ENTRENADORES/models"
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// GET ALL solicitudes (filtros: estado, especialidad)
+func GetAllSolicitudes(w http.ResponseWriter, r *http.Request) {
+	query := `SELECT id, usuario_id, gimnasio_id, nombres_apellidos, cedula, experiencia,
+		sobre_mi, especialidad, whatsapp, correo, direccion, estado, revisado_por,
+		revisado_en, creado_en, activo, fecha_modificacion, fecha_creacion
+		FROM solicitudes_entrenador WHERE 1=1`
+
+	estado       := r.URL.Query().Get("estado")
+	especialidad := r.URL.Query().Get("especialidad")
+
+	if estado != "" {
+		query += " AND estado ILIKE '%" + estado + "%'"
+	}
+	if especialidad != "" {
+		query += " AND especialidad ILIKE '%" + especialidad + "%'"
+	}
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var lista []models.SolicitudEntrenador
+	for rows.Next() {
+		var s models.SolicitudEntrenador
+		rows.Scan(
+			&s.ID, &s.UsuarioID, &s.GimnasioID, &s.NombresApellidos,
+			&s.Cedula, &s.Experiencia, &s.SobreMi, &s.Especialidad,
+			&s.Whatsapp, &s.Correo, &s.Direccion, &s.Estado,
+			&s.RevisadoPor, &s.RevisadoEn, &s.CreadoEn,
+			&s.Activo, &s.FechaModificacion, &s.FechaCreacion,
+		)
+		lista = append(lista, s)
+	}
+	respondJSON(w, 200, lista)
+}
