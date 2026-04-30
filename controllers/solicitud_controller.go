9536e7fee4dbdaffc6ea914a8315ac5651306b66
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:20:02 -0500

    se agrega func 14

--- controllers/solicitud_controller.go
+++ controllers/solicitud_controller.go
@@ -48,3 +48,32 @@ func GetAllSolicitudes(w http.ResponseWriter, r *http.Request) {
 	}
 	respondJSON(w, 200, lista)
 }
+
+
+func GetSolicitudByID(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var s models.SolicitudEntrenador
+
+	err := config.DB.QueryRow(
+		`SELECT id, usuario_id, gimnasio_id, nombres_apellidos, cedula, experiencia,
+		sobre_mi, especialidad, whatsapp, correo, direccion, estado, revisado_por,
+		revisado_en, creado_en, activo, fecha_modificacion, fecha_creacion
+		FROM solicitudes_entrenador WHERE id=$1`, id,
+	).Scan(
+		&s.ID, &s.UsuarioID, &s.GimnasioID, &s.NombresApellidos,
+		&s.Cedula, &s.Experiencia, &s.SobreMi, &s.Especialidad,
+		&s.Whatsapp, &s.Correo, &s.Direccion, &s.Estado,
+		&s.RevisadoPor, &s.RevisadoEn, &s.CreadoEn,
+		&s.Activo, &s.FechaModificacion, &s.FechaCreacion,
+	)
+
+	if err == sql.ErrNoRows {
+		respondJSON(w, 404, map[string]string{"error": "Solicitud no encontrada"})
+		return
+	}
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	respondJSON(w, 200, s)
+}
