fb5d3566f55855b5a72a40b631c5a1df853251c9
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:27:20 -0500

    se agrega func 19 (routes)

---
+++ routes/entrenador_routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"API_ENTRENADORES/controllers"
+	"github.com/gorilla/mux"
+)
+
+func RegisterEntrenadorRoutes(r *mux.Router) {
+	r.HandleFunc("/entrenadores", controllers.GetAllEntrenadores).Methods("GET")
+	r.HandleFunc("/entrenadores/{id}", controllers.GetEntrenadorByID).Methods("GET")
+	r.HandleFunc("/entrenadores", controllers.CreateEntrenador).Methods("POST")
+	r.HandleFunc("/entrenadores/{id}", controllers.UpdateEntrenador).Methods("PUT")
+	r.HandleFunc("/entrenadores/{id}", controllers.DeleteEntrenador).Methods("DELETE")
+}
