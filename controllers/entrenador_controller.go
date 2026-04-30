23cab606af46cf295b9931d26f827d5b254c4e92
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-04-30 15:16:10 -0500

    se agrega func 8

--- controllers/entrenador_controller.go
+++ controllers/entrenador_controller.go
@@ -16,3 +16,41 @@ func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(payload)
 }
+
+
+func GetAllEntrenadores(w http.ResponseWriter, r *http.Request) {
+	query := `SELECT id_entrenadores, id_adminitrador, cedula, descripcion, especialidad,
+		gimnasio_id, anios_experiencia, foto_url, aprovacion_entrenador, hoja_vida,
+		calificacion_prom, activo, fecha_modificacion, fecha_creacion
+		FROM entrenadores WHERE 1=1`
+
+	cedula       := r.URL.Query().Get("cedula")
+	especialidad := r.URL.Query().Get("especialidad")
+
+	if cedula != "" {
+		query += " AND cedula ILIKE '%" + cedula + "%'"
+	}
+	if especialidad != "" {
+		query += " AND especialidad ILIKE '%" + especialidad + "%'"
+	}
+
+	rows, err := config.DB.Query(query)
+	if err != nil {
+		respondJSON(w, 500, map[string]string{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var lista []models.Entrenador
+	for rows.Next() {
+		var e models.Entrenador
+		rows.Scan(
+			&e.IDEntrenadores, &e.IDAdministrador, &e.Cedula, &e.Descripcion,
+			&e.Especialidad, &e.GimnasioID, &e.AniosExperiencia, &e.FotoURL,
+			&e.AprovacionEntrenador, &e.HojaVida, &e.CalificacionProm,
+			&e.Activo, &e.FechaModificacion, &e.FechaCreacion,
+		)
+		lista = append(lista, e)
+	}
+	respondJSON(w, 200, lista)
+}
